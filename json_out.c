f6570488faada93c5759f17462cd8093a6397903
NGen15 <59732440+ngen15@users.noreply.github.com>
2023-10-14 14:36:15 +0200

    Update json_out.c

--- json_out.c
+++ json_out.c
@@ -671,12 +671,10 @@ char *sprintAircraftObject(char *p, char *end, struct aircraft *a, int64_t now, 
         }
 
         if (trackDataValid(&a->airground_valid) && a->airground == AG_GROUND) {
-            if (0)
                 p = safe_snprintf(p, end, ",\"ground\":true");
         } else {
             if (altBaroReliable(a))
                 p = safe_snprintf(p, end, ",\"alt_baro\":%d", a->baro_alt);
-            if (0)
                 p = safe_snprintf(p, end, ",\"ground\":false");
         }
     }
